d4b1c65251b448db27146368e63e683ace8ce2af
v.barcena.2020 <v.barcena.2020@alumnos.urjc.es>
2024-09-13 11:23:13 +0200

    new interface

--- src/v2/handTracking/symbols.py
+++ src/v2/handTracking/symbols.py
@@ -2,11 +2,12 @@ import cv2
 import numpy as np
 
 # SYMBOLS NEADED
-MODE_CHANGE_1 = [[1, 1, 0, 0, 0, 1, 1], [1, 1, 0, 0, 0, 0, 1]]
-MODE_CHANGE_2 = [[1, 1, 0, 0, 0, 0, 1], [1, 1, 0, 0, 0, 1, 1]]
-MOVE = 1
-NOT_MOVE = 0
+CHANGE_MODE_1 = [[1, 1, 0, 0, 0, 1, 1], [1, 1, 0, 0, 0, 0, 1]]
+CHANGE_MODE_2 = [[1, 1, 0, 0, 0, 0, 1], [1, 1, 0, 0, 0, 1, 1]]
+SYMBOLS_MODE = 0
+MOVE_MODE = 1
 FACE_MODE = 2
+MANUAL_MODE = 3
 
 PEACE = [0, 1, 1, 0, 0, 1, 1]
 METAL = [0, 1, 0, 0, 1, 1, 1]
@@ -81,11 +82,11 @@ def print_symbol(left_status, right_status, traces):
 
 
 def change_mode(left_status, right_status, mode):
-    if(left_status == MODE_CHANGE_1[0] and right_status == MODE_CHANGE_1[1]):
-        mode = MOVE
-    elif(left_status == MODE_CHANGE_2[0] and right_status == MODE_CHANGE_2[1]):
-        mode = NOT_MOVE
-    elif(left_status == MODE_CHANGE_2[0] and right_status == MODE_CHANGE_1[1]):
+    if(left_status == CHANGE_MODE_1[0] and right_status == CHANGE_MODE_1[1]):
+        mode = MOVE_MODE
+    elif(left_status == CHANGE_MODE_2[0] and right_status == CHANGE_MODE_2[1]):
+        mode = SYMBOLS_MODE
+    elif(left_status == CHANGE_MODE_2[0] and right_status == CHANGE_MODE_1[1]):
         mode = FACE_MODE
 
     return mode
